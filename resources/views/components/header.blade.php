0ac0e2b78b4b8d373bd9d51523dfdcc757399ac1
Xavi-brom <212230@student.aventus.nl>
2026-07-04 20:21:12 +0200

    home page -knoppen klaar en header werkt met switchen van dark/light mode

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,96 +1,95 @@
-props(['seetrough' => false, 'darkmode' => false, 'lightmode' => false])
+@php
+    $isHome = request()->routeIs('welcome');
+@endphp
 
-<div>
-    {{-- top part-header --}}
+<div class="{{ $isHome
+    ? 'absolute top-0 left-0 w-full z-50 bg-[#45454F]/50 border-b border-gray-500'
+    : 'bg-white dark:bg-[#45454F] border-b border-gray-100 dark:border-gray-500'}}">
+
+    {{-- Top part-header --}}
     <div class="flex items-center justify-between max-w-7xl mx-auto pt-6 pb-5">
-        {{-- logo --}}
-        <div class="shrink-0">
-            <img src="{{ asset('images/Hardbrass-white.png') }}" alt="Logo"
-                class="hidden dark:block h-8">
 
-            <img src="{{ asset('images/Hardbrass-black.png') }}" alt="Logo"
-                class="block dark:hidden h-8">
+        {{-- Logo --}}
+        <div>
+            @if($isHome)
+                <img src="{{ asset('images/Hardbrass-white.png') }}" alt="Logo" class="h-8">
+            @else
+                <img src="{{ asset('images/Hardbrass-white.png') }}" alt="Logo"
+                    class="hidden dark:block h-8">
+
+                <img src="{{ asset('images/Hardbrass-black.png') }}" alt="Logo"
+                    class="block dark:hidden h-8">
+            @endif
         </div>
 
-        {{-- search bar --}}
-        <div class="flex-1 max-w-xl mx-20">
-            <x-searchbar />
+        {{-- Search bar --}}
+        <div class="w-full max-w-md">
+            {{-- text + search icon --}}
+            <div class="flex items-center justify-between pb-3">
+                
+                {{-- text --}}
+                <div class="flex-1">
+                    <input
+                    type="text"
+                    placeholder='Zoek bijvoorbeeld naar "magneet sluiting"'
+                    class="w-full text-gray-800 dark:text-gray-500 placeholder-gray-200 dark:placeholder-gray-500 text-lg focus:outline-none"
+                    {{ $isHome
+                    ? 'text-white placeholder-white/70'
+                    : 'text-gray-800 dark:text-gray-200 placeholder-gray-400 dark:placeholder-gray-500'
+                    }}
+                    >
+                </div>
+                
+                {{-- search icon --}}
+                <div class="ml-3 shrink-0">
+                    <img
+                    src="{{ asset('images/search-icon.png') }}"
+                    alt="Search"
+                    class="h-5 w-5">
+                </div>
+            </div>
+            
+            {{-- underline --}}
+            <div class="{{ $isHome ? 'border-t-2 border-white/30' : 'border-t-2 border-gray-200 dark:border-gray-500'}}"></div>
         </div>
 
-        {{-- document icon --}}
-        <div class="shrink-0">
-            <img src="{{ asset('images/document-white.png') }}" alt="Logo"
-                class="hidden dark:block h-10">
+        {{-- Document icon --}}
+        <div>
+            @if($isHome)
+                <img src="{{ asset('images/document-white.png') }}" alt="Document" class="h-10">
+            @else
+                <img src="{{ asset('images/document-white.png') }}" alt="Document"
+                    class="hidden dark:block h-10">
 
-            <img src="{{ asset('images/document-black.png') }}" alt="Logo"
-                class="block dark:hidden h-10">
+                <img src="{{ asset('images/document-black.png') }}" alt="Document"
+                    class="block dark:hidden h-10">
+            @endif
         </div>
     </div>
 
-    {{-- divider --}}
-    <div class="max-w-7xl mx-auto border-t border-gray-200 dark:border-gray-500">
+    {{-- Divider --}}
+    <div class="max-w-7xl mx-auto {{ $isHome ? 'border-t border-white/20' : 'border-t border-gray-200 dark:border-gray-500' }}">
     </div>
 
-    {{-- bottom part-header --}}
-    <div class="flex justify-between items-center max-w-7xl mx-auto h-16 text-[16px] font-medium text-gray-800 dark:text-gray-100">
+    {{-- Bottom part-header --}}
+    <div class="{{ $isHome 
+    ? 'flex justify-between items-center max-w-7xl mx-auto h-16 text-[16px] font-medium text-white' 
+    : 'flex justify-between items-center max-w-7xl mx-auto h-16 text-[16px] font-medium text-gray-800 dark:text-gray-100' 
+    }}">
+
+        <a href="#" class="hover:text-[#d1b000]">Deurbeslag</a>
+
+        <a href="#" class="hover:text-[#d1b000]">Brievenbussen</a>
+
+        <a href="#" class="hover:text-[#d1b000]">Deursystemen</a>
+
+        <a href="#" class="hover:text-[#d1b000]">Catalogus</a>
+
+        <a href="#" class="hover:text-[#d1b000]">Projecten</a>
+
+        <a href="#" class="hover:text-[#d1b000]">Dealers</a>
+
+        <a href="#" class="hover:text-[#d1b000]">Contact</a>
+
     </div>
-</div>
-
-<div>
-    @if(seetrough)
-    <div class="bg-[#45454F]/10 backdrop-blur-sm">
-        {{-- top part-header --}}
-        <div class="">
-            <x-top-part-header />
-        </div>
-
-        {{-- divider --}}
-        <div>
-
-        </div>
-
-        {{-- bottom part-header --}}
-        <div>
-            <x-bottom-part-header />
-        </div>
-    </div>
-    @endif
-
-    @if(darkmode)
-    <div class="bg-[#45454F]">
-        {{-- top part-header --}}
-        <div>
-            <x-top-part-header />
-        </div>
-
-        {{-- divider --}}
-        <div>
-
-        </div>
-
-        {{-- bottom part-header --}}
-        <div>
-            <x-bottom-part-header />
-        </div>
-    </div>
-    @endif
-
-    @if(lightmode)
-    <div class="bg-white">
-        {{-- top part-header --}}
-        <div>
-            <x-top-part-header />
-        </div>
-
-        {{-- divider --}}
-        <div>   
-
-        </div>
-
-        {{-- bottom part-header --}}
-        <div>
-            <x-bottom-part-header />
-        </div>
-    </div>
-    @endif
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,8 @@
-<x-layout forceDark>
+<x-layout>
+    <img
+        src="{{ asset('images/background-image-home.png') }}"
+        alt="Welcome Image"
+        class="w-screen h-screen object-cover"
+    >
     <x-slot name="title">Welcome</x-slot>
-    <x-slot name="header">
-        <x-header class="bg-transparent backdrop-blur-m" />
-    </x-slot>
-    <div class="fixed inset-0 -z-10">
-        <img src="{{ asset('images/background-image-home.png') }}" alt="Welcome Image" class="w-full h-full object-cover">
-        <div class="absolute inset-0 shadow-[inset_0_0_80px_30px_rgba(0,0,0,0.6)] pointer-events-none"></div>
-    </div>
 </x-layout>
